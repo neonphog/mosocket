f86e2e3e021747d9d4edae0f9212df5735b06eb0
neonphog <neonphog@gmail.com>
2018-08-06 17:28:59 -0600

    checkpoint working on protocol

--- bin/test.js
+++ bin/test.js
@@ -10,20 +10,54 @@ mosocket.test().then(() => {}, (err) => {
 })
 */
 
-const { MultiAddr } = require('../lib/multiaddr')
+const { MoSocket } = require('../lib/mosocket')
+// const { MultiAddr } = require('../lib/multiaddr')
 const config = require('../lib/config')()
-const tcp = require('../lib/tcp')
+// const tcp = require('../lib/tcp')
 
 async function _main () {
+  const node1 = new MoSocket(config)
+  node1.on('bind', (addr) => {
+    console.log('node listening at', addr)
+  })
+
+  await node1.bind()
+
+  const addr = node1.getListeningAddrs()[0]
+  console.log('attempting to connect to', addr)
+
+  const node2 = new MoSocket(config)
+
+  await node2.connect(addr)
+
+  setTimeout(() => {
+    node1.close()
+    node2.close()
+  }, 1000)
+
+  /*
   const ma = new MultiAddr('/ip4/127.0.0.1/tcp/11011/udp/11012')
 
   const srv = await tcp.Listener.create(config, ma)
+  srv.on('connection', (con) => {
+    con.on('message', (msg) => {
+      console.log('msg:', msg.toString())
+    })
+    con.send(Buffer.from('test back'))
+  })
 
   const cli = await tcp.Connection.create(config, ma)
+  cli.on('message', (msg) => {
+    console.log('msg back:', msg.toString())
+  })
 
-  console.log('connected!')
-  cli.close()
-  srv.close()
+  cli.send(Buffer.from('hello'))
+
+  setTimeout(() => {
+    cli.close()
+    srv.close()
+  }, 1000)
+  */
 }
 
 _main().then(() => {}, (err) => {
